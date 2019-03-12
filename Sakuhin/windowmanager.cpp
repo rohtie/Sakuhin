65810358c365ab35218330f459cfdede656a68ec
Thor Merlin Lervik <rohtgnilk@gmail.com>
2019-03-12 13:37:36 +0100

    Add example window manager setups

--- Sakuhin/windowmanager.cpp
+++ Sakuhin/windowmanager.cpp
@@ -12,6 +12,60 @@ void WindowManager::initialize(const QSurfaceFormat &format, Backend* backend, S
     this->shadermanager = shadermanager;
     this->scenemanager = scenemanager;
 
+    /* Visuals mode
+    Window* previewWindow = new VisualsWindow();
+
+    previewWindow->initialize(backend, shadermanager, scenemanager, false, true);
+    previewWindow->setFormat(format);
+    previewWindow->resize(QSize(256, 256));
+    previewWindow->setPosition(0, 400);
+    previewWindow->setFlag(Qt::FramelessWindowHint);
+    previewWindow->show();
+
+    masterWindow = new VisualsWindow();
+    masterWindow->initialize(backend, shadermanager, scenemanager, true, false);
+    masterWindow->setFormat(format);
+    masterWindow->resize(QSize(512, 512));
+    masterWindow->setPosition(256, 0);
+    masterWindow->setFlag(Qt::FramelessWindowHint);
+    masterWindow->show();
+
+    windows.append(masterWindow);
+    windows.append(previewWindow);
+    /*--------------------------*/
+
+    /* Visuals projection mapping mode
+    masterWindow = new VisualsWindow();
+    masterWindow->setupMapping("data/config/dinzoil_F.json");
+    masterWindow->initialize(backend, shadermanager, true, false);
+    masterWindow->setFormat(format);
+    masterWindow->resize(QSize(555, 256));
+    masterWindow->setPosition(256, 512);
+    masterWindow->setFlag(Qt::FramelessWindowHint);
+    masterWindow->show();
+    windows.append(masterWindow);
+
+    Window* slaveWindow = new VisualsWindow();
+    slaveWindow->setupMapping("data/config/dinzoil_B.json");
+    slaveWindow->initialize(backend, shadermanager, false, false);
+    slaveWindow->setFormat(format);
+    slaveWindow->resize(QSize(555, 256));
+    slaveWindow->setPosition(811, 512);
+    slaveWindow->setFlag(Qt::FramelessWindowHint);
+    slaveWindow->show();
+    windows.append(slaveWindow);
+
+    Window* previewWindow = new VisualsWindow();
+    previewWindow->initialize(backend, shadermanager, false, true);
+    previewWindow->setFormat(format);
+    previewWindow->resize(QSize(256, 256));
+    previewWindow->setPosition(0, 512);
+    previewWindow->setFlag(Qt::FramelessWindowHint);
+    previewWindow->show();
+    windows.append(previewWindow);
+    /*--------------------------*/
+
+    // /* Timeline mode
     Window* previewWindow = new TimelineWindow();
 
     previewWindow->initialize(backend, shadermanager, scenemanager, false, true);
@@ -31,6 +85,7 @@ void WindowManager::initialize(const QSurfaceFormat &format, Backend* backend, S
 
     windows.append(masterWindow);
     windows.append(previewWindow);
+    /*--------------------------*/
 
     emit windowsChanged();
 }
